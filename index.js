679f2475c0caefc9b746b23305c76400a51efdcf
Camilo Pérez <camilo.perezvr@gmail.com>
2016-10-23 13:55:49 -0500

    Organizar estructura info

--- index.js
+++ index.js
@@ -1,6 +1,40 @@
 var fs = require('fs');
-var header = ['age','job_admin.','job_blue-collar','job_entrepreneur','job_housemaid','job_management','job_retired','job_self-employed','job_services','job_student','job_technician','job_unemployed','job_unknown','marital_divorced','marital_married','marital_single','marital_unknown', 'education_basic.4y','education_basic.6y','education_basic.9y','education_high.school','education_illiterate','education_professional.course','education_university.degree','education_unknown','default','housing','loan','contact_cellular','contact_telephone','month','day_of_week','duration','campaign','pdays','previous', 'poutcome_failure','poutcome_nonexistent','poutcome_success','y']
-var variables = ['age','admin.','blue-collar','entrepreneur','housemaid','management','retired','self-employed','services','student','technician','unemployed','unknown','divorced','married','single','unknown', 'basic.4y','basic.6y','basic.9y','high.school','illiterate','professional.course','university.degree','unknown','default','housing','loan','cellular','telephone','month','day_of_week','duration','campaign','pdays','previous', 'failure','nonexistent','success','y']
+var header = [
+	'age',
+	'job_admin.','job_blue-collar','job_entrepreneur','job_housemaid','job_management','job_retired','job_self-employed','job_services','job_student','job_technician','job_unemployed','job_unknown',
+	'marital_divorced','marital_married','marital_single','marital_unknown', 
+	'education_basic.4y','education_basic.6y','education_basic.9y','education_high.school','education_illiterate','education_professional.course','education_university.degree','education_unknown',
+	'default_no','default_yes','default_unknown',
+	'housing',
+	'loan',
+	'contact_cellular','contact_telephone',
+	'month',
+	'day_of_week',
+	'duration',
+	'campaign',
+	'pdays',
+	'previous',
+	'poutcome_failure','poutcome_nonexistent','poutcome_success',
+	'y'
+]
+var variables = [
+	'age',
+	'admin.','blue-collar','entrepreneur','housemaid','management','retired','self-employed','services','student','technician','unemployed','unknown',
+	'divorced','married','single','unknown',
+	'basic.4y','basic.6y','basic.9y','high.school','illiterate','professional.course','university.degree','unknown',
+	'no','yes','unknown',
+	'housing',
+	'loan',
+	'cellular','telephone',
+	'month',
+	'day_of_week',
+	'duration',
+	'campaign',
+	'pdays',
+	'previous',
+	'failure','nonexistent','success',
+	'y'
+]
 var finalObj = [[]];
 //fs.readFile('./data/bank/bank-full.csv','utf8',function done(err,data){
 fs.readFile('./data/bank-additional/bank-additional-full.csv','utf8',function done(err,data){
@@ -20,11 +54,12 @@ fs.readFile('./data/bank-additional/bank-additional-full.csv','utf8',function do
 			return finalLine;
 		}
 	);
-	var medsArray = fillSpaces(formatedData);
-	console.log('the meds is',medsArray);
-	//processData(formatedData);
+	//var medsArray = fillSpaces(formatedData);
+	//console.log('the meds is',medsArray);
+	processData(formatedData);
 });
 
+//Metodo para hacer imputacion de variables obteniendo las medias 
 var fillSpaces = function(formatedData){
 	var i, j, meds = [], medsFinal = [];
 	for(i in formatedData){
@@ -51,7 +86,6 @@ var fillSpaces = function(formatedData){
 			}
 		}
 	}
-	console.log('the meds is',meds);
 	return medsFinal;
 };
 var processData = function (formatedData){
@@ -61,7 +95,6 @@ var processData = function (formatedData){
 		if(i == 0){
 			continue;
 		}
-		console.log('i',i)
 		finalObj[i] = [];
 		finalObj[i][0] = formatedData[i][0];
 		//setWork 1 - 12
@@ -72,7 +105,6 @@ var processData = function (formatedData){
 		finalObj[i] = setVariable(17,24,formatedData[i][3],finalObj[i])
 
 	}
-	console.log('the final is',finalObj)
 };
 
 var setVariable = function (ini,end,activity,retObj){
